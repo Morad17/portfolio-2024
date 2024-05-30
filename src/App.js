e76ed84b1c50a73a63d0b90f77b72d3edb40a653
Morad17 <morad123@hotmail.co.uk>
2024-05-30 15:39:56 +0100

    creating navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './assets/scss/base.scss'
+import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
 
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Home from './pages/Home'
 function App() {
+
+  const Layout = () => {
+    return (<div className="main-layout">
+      <Navbar/>
+      <Outlet />
+      <Footer />
+    </div>)
+  }
+
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Layout />,
+      children: [{
+        path: "/",
+        element: <Home />
+      },
+    ]}
+  ])
+
   return (
     <div className="App">
-    
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import About from '../assets/icons/about.png'
+import Contact from '../assets/icons/contact.png'
+import Home from '../assets/icons/home.png'
+import Projects from '../assets/icons/projects.png'
+import Skills from '../assets/icons/skills.png'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <ul>
+        <li>
+          <Link to="/" className="nav-link">
+            <img src={Home} alt="" />
+            <p>Home</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-link">
+            <img src={About} alt="" />
+            <p>About</p>
+          </Link>
+        </li>
+        <li>
+        <Link to="/" className="nav-link">
+            <img src={Projects} alt="" />
+            <p>Projects</p>
+          </Link>
+
+        </li>
+        <li>
+          <Link to="/" className="nav-link">
+            <img src={Skills} alt="" />
+            <p>Skills</p>
+          </Link>
+        </li>
+        <li>
+          <Link to="/" className="nav-link">
+            <img src={Contact} alt="" />
+            <p>Contact</p>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
