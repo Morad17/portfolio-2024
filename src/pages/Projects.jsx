cfb4225f902e1e3cb9e6d972f219d13b06501264
Morad17 <morad123@hotmail.co.uk>
2024-06-05 16:34:45 +0100

    updates

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,47 @@
 import React from 'react'
+import travelThumb from "../assets/images/travel-info-thumb.jpg"
+import holidayPhotoThumb from "../assets/images/holiday-photo-thumb.png"
 
 const Projects = () => {
   return (
-    <div className="projects">
+    <div className="projects-section">
+      
+      <section className="project-section">
       <h1 className="projects-title">
         Projects
       </h1>
+        <div className="project-slides">
+          <div className="project-1">
+          <img src={travelThumb} alt="" className="project-image"/>
+          <div className="project-text">
+            <h3>Travel Adviser Guide</h3>
+            <p>React | Google Maps Api</p>
+          </div>
+          </div>
+          <div className="project-2">
+            <img src={holidayPhotoThumb} alt="" className="project-image"/>
+            <div className="project-text">
+              <h3>Holiday Photo Gallery</h3>
+              <p>React | Firebase</p>
+            </div>
+          </div>
+          <div className="project-2">
+            <img src={holidayPhotoThumb} alt="" className="project-image"/>
+            <div className="project-text">
+              <h3>Holiday Photo Gallery</h3>
+              <p>React | Firebase</p>
+            </div>
+          </div>
+          <div className="project-2">
+            <img src={holidayPhotoThumb} alt="" className="project-image"/>
+            <div className="project-text">
+              <h3>Holiday Photo Gallery</h3>
+              <p>React | Firebase</p>
+            </div>
+          </div>
+        </div>
+        
+      </section>
     </div>
   )
 }
