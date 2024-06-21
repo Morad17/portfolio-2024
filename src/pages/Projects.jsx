aa351527c96dd9879b04d29e53309bbac54bfeea
Morad17 <morad123@hotmail.co.uk>
2024-06-21 11:53:02 +0100

    reorganising projects & nav

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,48 +5,79 @@ import holidayPhotoThumb from "../assets/images/holiday-photo-thumb.png"
 const Projects = () => {
   return (
     <div className="projects-section" id="projects"> 
-      <section className="info-section-left">
-        
-      </section>     
-      <section className="project-content">
       <h1 className="projects-title">
         Projects
       </h1>
+      <div className="main-content">
+        <section className="info-section-left">
         <div className="project-slides">
-          <div className="project-1">
-          <img src={travelThumb} alt="" className="project-image"/>
-          <div className="project-text">
-            <h3>Travel Adviser Guide</h3>
-            <p>React | Google Maps Api</p>
-          </div>
-          </div>
-          <div className="project-2">
-            <img src={holidayPhotoThumb} alt="" className="project-image"/>
-            <div className="project-text">
-              <h3>Holiday Photo Gallery</h3>
-              <p>React | Firebase</p>
+            <div className="project-1">
+              <img src={travelThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Travel Adviser Guide</h3>
+                <p>React | Google Maps Api</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
             </div>
           </div>
-          <div className="project-2">
-            <img src={holidayPhotoThumb} alt="" className="project-image"/>
-            <div className="project-text">
-              <h3>Holiday Photo Gallery</h3>
-              <p>React | Firebase</p>
+        </section>     
+        <section className="project-content">
+          <div className="project-slides">
+            <div className="project-1">
+              <img src={travelThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Travel Adviser Guide</h3>
+                <p>React | Google Maps Api</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
+            </div>
+            <div className="project-2">
+              <img src={holidayPhotoThumb} alt="" className="project-image"/>
+              <div className="project-text">
+                <h3>Holiday Photo Gallery</h3>
+                <p>React | Firebase</p>
+              </div>
             </div>
           </div>
-          <div className="project-2">
-            <img src={holidayPhotoThumb} alt="" className="project-image"/>
-            <div className="project-text">
-              <h3>Holiday Photo Gallery</h3>
-              <p>React | Firebase</p>
-            </div>
-          </div>
-        </div>
-        
-      </section>
-      <section className="info-section-right">
+          
+        </section>
+        <section className="info-section-right">
 
-      </section>
+        </section>
+      </div>
     </div>
   )
 }
