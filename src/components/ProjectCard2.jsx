71073c2a3cabb9791bbfdc6a38934dc72ec18b1d
Morad17 <morad123@hotmail.co.uk>
2024-06-22 14:08:20 +0100

    updates

--- src/components/ProjectCard2.jsx
+++ src/components/ProjectCard2.jsx
@@ -3,11 +3,16 @@ import travelThumb from "../assets/images/travel-info-thumb.jpg"
 
 const ProjectCard2 = ({data:{title, techs,image}}) => {
   return (
-    <div className="project-card-2" style={{backgroundImage:`url(${image})`}}>
-        {/* <img src={image} alt="" className="project-image"/> */}
+    <div className="project-card-2" >
+        <img src={image} alt="" className="project-image"/>
         <h3 className="project-title">{title}</h3>
         <p className="project-tech">{techs}</p>
     </div>
+    // <div className="project-card-2" style={{backgroundImage:`url(${image})`}}>
+    // {/* <img src={image} alt="" className="project-image"/> */}
+    // <h3 className="project-title">{title}</h3>
+    // <p className="project-tech">{techs}</p>
+    // </div>
   )
 }
 
