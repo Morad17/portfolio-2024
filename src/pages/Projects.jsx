fbe6f519f96033ea1c36a4dfc763b89a91a75405
Morad17 <morad123@hotmail.co.uk>
2024-08-27 12:24:58 +0100

    updates

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useAnimate } from 'framer-motion'
+
 import ProjectCard1 from '../components/ProjectCard1'
 import ProjectCard2 from '../components/ProjectCard2'
 
@@ -6,15 +8,20 @@ import travelThumb from "../assets/images/travel-info-thumb.jpg"
 import holidayPhotoThumb from "../assets/images/holiday-photo-thumb.png"
 import {data }from "../assets/data/projects"
 
-import { useAnimate } from 'framer-motion'
+
 
 const Projects = () => {
 
+  const [animateOn, turnAnimateOn ] = useState(true)
+
+  //Animations//
   const [scope, animate ] = useAnimate()
 
   const handleAnimate = async (id) => {
-    animate(`#${id}`, {x: "30vw"})
+   await animate("#project-content", {x: "30vw"}, {duration: 1})
+   await animate("#animated-content", {display: "flex"},)
   }
+  
 
   return (
     <div ref={scope} className="projects-section" id="projects"> 
@@ -22,65 +29,54 @@ const Projects = () => {
         Projects
       </h1>
       <div className="main-content">
-        {/* <section className="info-section-left">
-          <div className="project-slides">
-           {
-            data.map((data,key)=> {
-              if (data.featured === true) {
-                return <ProjectCard2 data={data} />
-              } else {
-                return <ProjectCard1 data={data} />
-              }
-            })
-           }
+        <section id="animated-content" className="animated-content">
+          <div className="content-left">
+            <div className="animated-card">
+              <img src={travelThumb} alt="" />
+            </div>
+
           </div>
-        </section>*/}
-        <section className="project-content">
+          <div className="content-right">
+
+          </div>
+        </section>
+        <section id="project-content" className="project-content">
           <div className="project-slides">
-            <div id="project-1" className="project-1">
+            <div id="project-1" className="project-1 project-card">
               <a onClick={() => handleAnimate("project-1")}>
-                 <img src={travelThumb} alt="" className="project-image"/>
+                <img src={travelThumb} alt="" className="project-image"/>
                 <div className="card-overlay">
-                <div className="project-text">
-                <h3>Travel Adviser Guide</h3>
-                <p>React | Google Maps Api</p>
-              </div>
+                  <div className="project-text">
+                    <h3>Travel Adviser Guide</h3>
+                    <p>React | Google Maps Api</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div id="project-2" className="project-2 project-card">
+              <a onClick={() => handleAnimate("project-2")}>
+                <img src={holidayPhotoThumb} alt="" className="project-image"/>
+                <div className="card-overlay">
+                  <div className="project-text">
+                    <h3>Holiday Photo Gallery</h3>
+                    <p>React | Firebasei</p>
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div id="project-3" className="project-3 project-card">
+              <a onClick={() => handleAnimate("project-3")}>
+                <img src={holidayPhotoThumb} alt="" className="project-image"/>
+                <div className="card-overlay">
+                  <div className="project-text">
+                    <h3>Holiday Photo Gallerye</h3>
+                    <p>React | Firebase</p>
+                  </div>
               </div>
               </a>
-             
-              
-            </div>
-            <div id="project-2" className="project-2">
-              <img src={holidayPhotoThumb} alt="" className="project-image"/>
-              <div className="card-overlay">
-                <div className="project-text">
-                  <h3>Holiday Photo Gallery</h3>
-                  <p>React | Firebase</p>
-                </div>
-              </div>
-            </div>
-            <div className="project-2">
-              <img src={holidayPhotoThumb} alt="" className="project-image"/>
-              <div className="card-overlay">
-                <div className="project-text">
-                  <h3>Holiday Photo Gallery</h3>
-                  <p>React | Firebase</p>
-                </div>
-              </div>
-            </div>
-            <div className="project-2">
-              <img src={holidayPhotoThumb} alt="" className="project-image"/>
-              <div className="card-overlay">
-                <div className="project-text">
-                  <h3>Holiday Photo Gallery</h3>
-                  <p>React | Firebase</p>
-                </div>
-              </div>
             </div>
           </div>
         </section>
-        {/* <section className="info-section-right">
-        </section> */}
       </div>
     </div>
   )
